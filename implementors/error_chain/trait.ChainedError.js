bc5a71b50f2453752bfc843f152c19c2ca4ebbbd
Travis CI User <travis@example.org>
2018-04-05 19:17:24 +0000

    Update documentation

---
+++ implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["africastalking_gateway"] = ["impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"africastalking_gateway/struct.Error.html\" title=\"struct africastalking_gateway::Error\">Error</a>",];
+implementors["error_chain"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/hyper/header/trait.Header.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["africastalking_gateway"] = ["impl <a class=\"trait\" href=\"hyper/header/trait.Header.html\" title=\"trait hyper::header::Header\">Header</a> for <a class=\"struct\" href=\"africastalking_gateway/struct.Apikey.html\" title=\"struct africastalking_gateway::Apikey\">Apikey</a>",];
+implementors["hyper"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
